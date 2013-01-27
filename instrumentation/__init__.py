8640f491aafe7995c95baee8365b8296f787098c
henrietta <piotr.maslanka@henrietta.com.pl>
2013-01-27 21:24:35 +0100

    snapshots removed pt 2, interface additions

---
+++ instrumentation/__init__.py
@@ -0,0 +1,4 @@
+from satella.instrumentation.insmgr import InstrumentationManager
+from satella.instrumentation.basecounter import InstrumentationCounter
+
+from satella.instrumentation.exceptions import CounterExists, CounterNotExists, NoData
