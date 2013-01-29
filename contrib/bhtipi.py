0cc072022d82c6fa9d9583defbd8c9105ad10e61
henrietta <piotr.maslanka@henrietta.com.pl>
2013-01-29 19:49:22 +0100

    descriptions and unit outputted too

--- contrib/bhtipi.py
+++ contrib/bhtipi.py
@@ -22,14 +22,26 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 # -------------------------------------------------- Nested rendering of the counter infrastructure
 
 def render_counter(cc, nestlevel):
-
     try:
         val = cc.get_current()
     except NoData:
         val = '<span class="nodata">no data</span>'
 
-    return """<div class="counter"><div class="counter_label">%s</div><div class="counter_value">%s</div></div>""" \
-            % (cc.name, val)
+    s = ['<div class="counter"><div class="counter_label']
+    if cc.description != None:
+        desc = cc.description if type(cc.description) == str else cc.description.encode('utf8')
+        s.append(' has_descr" title="%s"' % desc)
+
+    s.append('">%s</div><div class="counter_value">%s</div>' % (cc.name, val))
+
+    if cc.units != None:
+        units = cc.units if type(cc.units) == str else cc.units.encode('utf8')
+
+        s.append('<div class="counter_units">%s</div>' % units)
+
+    s.append('</div>')
+
+    return ''.join(s)
 
 def render_collection(cc, nestlevel):
     u = """<h%s>%s</h%s><hr><div class="indent">""" % (nestlevel, cc.name, nestlevel)
@@ -71,8 +83,13 @@ class BHTIPIRequestHandler(BaseHTTPRequestHandler):
             margin-right: 10px;
             color: #A9A9A9;
         }
+            .has_descr { text-decoration: underline; }
         .counter_value {
-            width: 100px;
+            margin-right: 20px
+        }
+        .counter_units {
+            color: gray;
+            font-style: italic;
         }
         .nodata { color: gray; }
         .indent { margin-left: 10px; }
