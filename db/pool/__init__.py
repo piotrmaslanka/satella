71a8818a025484358f929a0c7b7a89b281ef455d
henrietta <piotr.maslanka@henrietta.com.pl>
2013-02-09 22:41:20 +0100

    docstring updates

--- db/pool/__init__.py
+++ db/pool/__init__.py
@@ -22,7 +22,6 @@ class DatabaseDefinition(object):
         self.xwcb = xwcb
         self.get_connection = lambda: cccall(*cccall_args, **cccall_kwargs) #: closure that returns a connection
 
-
 class ConnectionPool(object):
     def __init__(self, dd, connections=1):
         """
@@ -40,7 +39,9 @@ class ConnectionPool(object):
             self.connections.put(self.dd.get_connection())
 
     def close(self):
-        """Closes all connections"""
+        """Closes all connections in this connection pool.
+        Use only when you are sure that no-one is using the
+        connection pool anymore"""
         while True:
             try:
                 k = self.connections.get(False)
@@ -49,7 +50,8 @@ class ConnectionPool(object):
             k.close()
 
     def get_connection(self):
-        """Gets a connection from the pool. May block"""
+        """Gets a connection from the pool. May block
+        @return: a connection object"""
         return self.connections.get(True)
 
     def put_connection(self, conn):
@@ -59,13 +61,17 @@ class ConnectionPool(object):
 
     def invalidate_connection(self, conn):
         """
+        Marks off a connection as invalid. It will not be given
+        via get_connection() anymore.
+
         @param conn: connection object to be considered invalidated
         """
         conn.close()
         self.connections.put(self.dd.get_connection())
 
     def cursor(self):
-        """Returns a new cursor, constructed from a connection 
+        """
+        Returns a new cursor, constructed from a connection 
         picked from the pool. Cursor has the "regenerative" abilities
         that this pool supports, ie. it will be recreated if it is 
         detected that on a query connection fails.
@@ -73,25 +79,55 @@ class ConnectionPool(object):
         of transaction", first invoke a dummy SQL statement (ie. SELECT 2+2).
 
         If this cursor does a query and it fails (disconnect) it will repeat that again.
-        It is better to repeat SELECT 2+2 that some important transaction's command set"""
+        It is better to repeat SELECT 2+2 that some important transaction's command set.
+        Check your database transaction semantics on 'connection dropped'. You have been warned.
+
+        Use it like that:
+
+            cp = ConnectionPool(some_db_definition, some_connection_amount)
+            with cp.cursor() as cur:
+                cur.execute('DO SQL')
+            # at this point cursor gets closed and underlying connection is 
+            # returned to the pool.
+        """
 
         class Cursor(object):
+            """
+            Cursor wrapper object. Provides automatic reconnect on failed
+            execute().
+
+            Supports the context manager protocol.
+            """
             def __init__(self, cp):
-                """The wrapper"""
+                """
+                Initializes the cursor wrapper.
+
+                Cursor will close() itself on destructor if not closed
+                by that time.
+
+                @param cp: master L{ConnectionPool} object
+                @type: L{ConnectionPool}
+                """
                 self.cp = cp
                 self.conn = self.cp.get_connection()
                 self.cursor = self.conn.cursor()
+                self.cleaned_up = False
 
             def __reconnect(self):
+                """Regenerates internal connection and cursor"""
                 self.cp.invalidate_connection(self.conn)
                 self.conn = cp.get_connection()
                 self.cursor = self.conn.cursor()
 
             def close(self):
-                """Closes the cursor, returns the connection
+                """Closes the cursor, returns the connection.
                 to the pool"""
                 self.cursor.close()
                 self.cp.put_connection(self.conn)
+                self.cleaned_up = True
+
+            def __del__(self):
+                if not self.cleaned_up: self.close()
 
             def __enter__(self):
                 return self
