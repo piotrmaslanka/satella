7d506daa9462a8d311e1b616eb14f89463cd549a
henrietta <piotr.maslanka@henrietta.com.pl>
2013-02-13 03:58:13 +0100

    semantics changed + doc for .filter

--- instrumentation/logging/logentrysetinterface.py
+++ instrumentation/logging/logentrysetinterface.py
@@ -19,18 +19,21 @@ class LogEntrySetInterface(object):
         Filters the result set according to kwargs keys and values.
         Because of how backends may differ, this is expected to minimally implement
         following keys (for kwargs):
-        - when_lt   -   .when less than value
-        - when_le   -   .when less or equal than value
-        - when_gt   -   .when greater than value
-        - when_ge   -   .when greater or equal than value
+        - when__lt   -   .when less than value
+        - when__le   -   .when less or equal than value
+        - when__gt   -   .when greater than value
+        - when__ge   -   .when greater or equal than value
         - who_hierarchy - either .who is equal to value, or if you take away the value
                           from .who, then the first character will be a dot
         - who       -   .who equal to value
-        - tags_in   -   intersection of .tags and value is nonempty
+        - tags__in   -   intersection of .tags and value is nonempty
         - tags      -   .tags equal to value
 
         Returns a L{LogSetInterface} implementation that will not include filtered out values
-        shall it's .events() be called
+        shall it's .events() be called.
+
+        If an implementation-specified order of .filter() calls or simultaneous kwargs entries
+        is not supported, .filter is to raise ValueError.
         """
         raise RuntimeError, 'abstract'
 
