2c41b2ee6e20e9313fd6d585b9d2d7ee67d1eba2
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2014-09-03 12:44:25 +0200

    logging subsystem removed
    superficial

--- instrumentation/logging/__init__.py
+++
@@ -1,2 +0,0 @@
-from satella.instrumentation.logging.logentry import LogEntry
-from satella.instrumentation.logging.loggerinterface import LoggerInterface
--- instrumentation/unittests/__init__.py
+++ instrumentation/unittests/__init__.py
@@ -4,5 +4,4 @@ from pulsecounter import PulseCounterTest
 from countercollection import CounterCollectionTest
 from deltacounter import DeltaCounterTest
 from exctrack import TrackbackTest
-from logging import LoggingTest
 from stringvaluecounter import StringValueCounter
