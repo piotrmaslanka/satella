49522f1635854fdf6d3504cf243cf7f92287474c
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-07-08 05:15:40 +0200

    unnecessary warning

--- satella/posix/__init__.py
+++ satella/posix/__init__.py
@@ -8,24 +8,17 @@ import logging
 import os
 import warnings
 
-try:
-    from satella.posix.daemon import daemonize
-    from satella.posix.pidlock import AcquirePIDLock
+
+from satella.posix.daemon import daemonize
+from satella.posix.pidlock import AcquirePIDLock
 
 
-    def is_running_as_root():
-        """
-        Is this process running as root?
+def is_running_as_root():
+    """
+    Is this process running as root?
 
-        Checks whether EUID is 0
-        :return: bool
-        """
-        return os.geteuid() == 0
-
-
-except ImportError:
-    warnings.warn(u'Not running on POSIX, some functionality is unavailable')
-
-
-
+    Checks whether EUID is 0
+    :return: bool
+    """
+    return os.geteuid() == 0
 
