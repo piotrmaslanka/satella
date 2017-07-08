51251604a6817460da049b43a5243d35d9cead75
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-07-09 00:31:56 +0200

    fix

--- satella/posix/__init__.py
+++ satella/posix/__init__.py
@@ -5,9 +5,7 @@ UNIX things
 from __future__ import print_function, absolute_import, division
 import six
 import logging
-import functools
 import os
-import warnings
 from .daemon import daemonize
 from .pidlock import AcquirePIDLock
 from .signals import hang_until_sig
@@ -25,19 +23,3 @@ def is_running_as_root():
     """
     return os.geteuid() == 0
 
-
-
-try:
-    os.fork
-except AttributeError:
-    logger.warn('Running on Windows, skipping fork')
-else:
-    @functools.wraps(os.fork)
-    def _os_path_monkey_patch():
-        # os.fork() is replaced with that
-        _myself.on_before_fork()
-        pid = os.fork()
-        _myself.on_after_fork(pid == 0)  # on_after_fork(is_child::bool)
-        return pid
-
-    os.fork = _os_path_monkey_patch
