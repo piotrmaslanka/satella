ff40e64f45fcd0836d36a233b4f90181186c9f30
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-08-14 10:44:37 +0200

    bf

--- satella/coding/typecheck.py
+++ satella/coding/typecheck.py
@@ -278,7 +278,7 @@ def typed(*t_args, **t_kwargs):
         @functools.wraps(fun)
         def inner(*args, **kwargs):
 
-            if isinstance(fun, types.MethodType) or inspect.ismethod(fun):   # instancemethod or classmethod
+            if inspect.ismethod(fun):   # instancemethod or classmethod
                 cargs = args[1:]
             else:
                 cargs = args
