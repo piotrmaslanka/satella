ef9adfb0585261bffa5ba4ab28fde94940b89310
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-10-07 22:29:11 +0200

    trove -> setup.cfg

--- setup.py
+++ setup.py
@@ -11,19 +11,5 @@ setup(keywords=['ha', 'high availability', 'scalable', 'scalability', 'server'],
       tests_require=[
           "nose", "mock", "coverage"
       ],
-      test_suite='nose.collector',
-      classifiers=[
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.4',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: Implementation :: CPython',
-            'Programming Language :: Python :: Implementation :: PyPy',
-            'Operating System :: POSIX',
-            'Operating System :: OS Independent',
-            'Development Status :: 2 - Pre-Alpha',
-            'License :: OSI Approved :: MIT License',
-            'Topic :: Software Development :: Libraries'
-
-      ]
+      test_suite='nose.collector'
     )
