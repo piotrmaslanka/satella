d696145b2f6023a0c2b5c3f01612a389823090f1
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2018-02-16 23:40:51 +0100

    fix imports

--- satella/coding/typecheck/__init__.py
+++ satella/coding/typecheck/__init__.py
@@ -21,7 +21,5 @@ from .decorators import __all__ as __all_decorators__
 from .argparse import __all__ as __all_argparse__
 from .basics import __all__ as __all_basics__
 
-__all__ = __all_decorators__ + \
-          __all_argparse__ + \
-          filter(lambda p: not p.startswith('_'), __all_basics__)
-
+__all__ = filter(lambda p: not p.startswith('_'),
+                 __all_decorators__ + __all_argparse__ + __all_basics__)
