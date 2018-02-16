2a48f403d08bc11d6bcbdbe0dc571e5f9e865443
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2018-02-16 23:22:35 +0100

    optimize imports

--- satella/coding/__init__.py
+++ satella/coding/__init__.py
@@ -4,14 +4,14 @@ Just useful objects to make your coding nicer every day
 """
 from __future__ import print_function, absolute_import, division
 
-from .concurrent import Monitor, RMonitor, CallableGroup
 from .algos import merge_dicts
+from .concurrent import Monitor, RMonitor, CallableGroup
 from .recast_exceptions import rethrow_as, silence_excs
+from .structures import TimeBasedHeap, Heap, typednamedtuple, OmniHashableMixin
 from .typecheck import typed, Callable, Sequence, \
     TypeVar, Mapping, Iterable, Any, Optional, CallSignature, \
     Number, coerce, Set, Dict, List, Tuple, checked_coerce, for_argument, \
     precondition, PreconditionError
-from .structures import TimeBasedHeap, Heap, typednamedtuple, OmniHashableMixin
 
 __all__ = [
     'typednamedtuple', 'OmniHashableMixin'
