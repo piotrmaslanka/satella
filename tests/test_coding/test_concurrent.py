4aead6ce6a7a6fc1d28d4acf31d972c3e1aa6840
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2018-02-17 01:32:21 +0100

    test for concurrent

---
+++ tests/test_coding/test_concurrent.py
@@ -0,0 +1,27 @@
+# coding=UTF-8
+from __future__ import print_function, absolute_import, division
+import six
+import unittest
+
+from satella.coding import CallableGroup
+
+
+class TestCallableGroup(unittest.TestCase):
+    def test_callable_group(self):
+
+        a = {
+            'a': False,
+            'b': False
+        }
+
+        opF = lambda what: lambda: a.__setitem__(what, True)
+
+        cg = CallableGroup()
+
+        cg.add(opF('a'))
+        cg.add(opF('b'))
+
+        cg()
+
+        self.assertTrue(all(a.values()))
+
