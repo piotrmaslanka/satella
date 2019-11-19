b83f23cf70b5dfdcc9f48cb670c06479d0a5be94
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-11-19 18:26:57 +0100

    added a locked dataset data type

--- satella/coding/concurrent/__init__.py
+++ satella/coding/concurrent/__init__.py
@@ -3,3 +3,6 @@ from __future__ import absolute_import
 
 from .callablegroup import *
 from .monitor import *
+from .locked_dataset import LockedDataset
+
+__all__ = ['LockedDataset', 'Monitor', 'RMonitor', 'CallableGroup']
