5733bac2e9e47f6270aaa0fb5f80fa9ac202598b
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-12-06 22:25:45 +0100

    smarter tests

--- satella/instrumentation/dump_frames_on.py
+++ satella/instrumentation/dump_frames_on.py
@@ -7,17 +7,19 @@ from satella.instrumentation import Traceback
 logger = logging.getLogger(__name__)
 
 
+def dump_frames_on(sig_no, stack_frame, output):
+    sys.stderr.write("Stack frame dump requested\n")
+    # noinspection PyProtectedMember
+    for frame in sys._current_frames():
+        sys.stderr.write("For stack frame %s" % (repr(frame),))
+        tb = Traceback(frame)
+        tb.pretty_print(output=output)
+    sys.stderr.write("End of stack frame dump\n")
+
+
 def install_dump_frames_on(signal_number, output=sys.stderr):
     """
     Instruct Python to dump all frames onto output, along with their local variables
     upon receiving given signal
     """
-    def dump_frames_on(sig_no, stack_frame):
-        sys.stderr.write("Stack frame dump requested\n")
-        # noinspection PyProtectedMember
-        for frame in sys._current_frames():
-            sys.stderr.write("For stack frame %s" % (repr(frame), ))
-            tb = Traceback(frame)
-            tb.pretty_print(output=output)
-        sys.stderr.write("End of stack frame dump\n")
-    signal.signal(signal_number, dump_frames_on)
+    signal.signal(signal_number, lambda sig_no, stack_frame: dump_frames_on(sig_no, stack_frame, output))
--- tests/test_exception_handling/test_dump_frames_on.py
+++ tests/test_exception_handling/test_dump_frames_on.py
@@ -4,12 +4,10 @@ import unittest
 import os
 import signal
 import sys
-from satella.instrumentation import install_dump_frames_on
+from satella.instrumentation.dump_frames_on import dump_frames_on
 logger = logging.getLogger(__name__)
 
 
 class TestDumpFramesOn(unittest.TestCase):
-    @unittest.skipIf('win' in sys.platform, 'Running on Windows')
     def test_dump_frames_on(self):
-        install_dump_frames_on(signal.SIGUSR2)
-        os.kill(0, int(signal.SIGUSR2))
+        dump_frames_on(0, None, sys.stderr)
