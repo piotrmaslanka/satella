d5faf68dcf63d47806a1f7ec0dcd330bc37d4670
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2019-12-06 22:14:49 +0100

    code cleanup, bugfix

--- satella/__init__.py
+++ satella/__init__.py
@@ -1,3 +1,3 @@
 # coding=UTF-8
-__version__ = '2.1.2a4'
+__version__ = '2.1.2a5'
 
--- satella/instrumentation/dump_frames_on.py
+++ satella/instrumentation/dump_frames_on.py
@@ -1,8 +1,9 @@
 import logging
-import typing as tp
 import sys
-logger = logging.getLogger(__name__)
 from satella.instrumentation import Traceback
+
+
+logger = logging.getLogger(__name__)
 
 
 def install_dump_frames_on(signal, output=sys.stderr):
@@ -11,10 +12,11 @@ def install_dump_frames_on(signal, output=sys.stderr):
     upon receiving given signal
     """
     def dump_frames_on(sig_no, stack_frame):
-
         sys.stderr.write("Stack frame dump requested\n")
+        # noinspection PyProtectedMember
         for frame in sys._current_frames():
             sys.stderr.write("For stack frame %s" % (repr(frame), ))
             tb = Traceback(frame)
-            tb.pretty_print(output=sys.stderr)
+            tb.pretty_print(output=output)
+        sys.stderr.write("End of stack frame dump\n")
     signal.signal(signal, dump_frames_on)
