2690345d6b5eb941c80e024421a2e53c0b76a77d
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-13 18:57:30 +0100

    automatic build will occur only on submitted tags

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-if [ $TRAVIS_BRANCH == "master" ]; then
+if [ ! -z "$TRAVIS_TAG" ]; then
     pip install wheel twine
     python setup.py bdist bdist_wheel
     twine upload -u $PYPI_USER -p $PYPI_PWD dist/*
