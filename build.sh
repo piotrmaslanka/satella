cc9f842ee5c4b16c513d50d09efed73fe8f566a4
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-13 19:01:16 +0100

    build on master

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-if [ ! -z "$TRAVIS_TAG" ]; then
+if [ $TRAVIS_BRANCH == "master" ]; then
     pip install wheel twine
     python setup.py bdist bdist_wheel
     twine upload -u $PYPI_USER -p $PYPI_PWD dist/*
