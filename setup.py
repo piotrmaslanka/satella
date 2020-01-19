1f72f8b8dd95f361c375d7da51d59cbf64c60ced
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-01-19 16:10:10 +0100

    use nose2 for tests

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(keywords=['ha', 'high availability', 'scalable', 'scalability', 'server'],
             'psutil'
       ],
       tests_require=[
-          "nose", "mock", "coverage"
+          "nose2", "mock", "coverage"
       ],
-      test_suite='nose.collector',
+      test_suite='nose2.collector.collector',
       python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       )
