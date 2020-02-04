9ab5d96cacd6fd6be0af1b557bef93f2341ff877
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-04 19:50:35 +0100

    fix test for Python 3.5

--- tests/test_instrumentation/test_metrics/test_exporters.py
+++ tests/test_instrumentation/test_metrics/test_exporters.py
@@ -8,7 +8,9 @@ class TestExporters(unittest.TestCase):
 
         a = {'root': {'metric': [{'k': 2, '_': 3, 'm': '"'}, {'k': 4, '_': 6}]}}
         b = json_to_prometheus(a)
-        self.assertEqual("""root_metric{k=2, m="\""} 3
+        self.assertTrue(b == """root_metric{k=2, m="\""} 3
 root_metric{k=4} 6
-""", b)
+""" or b == """root_metric{m="\"", k=2} 3
+root_metric{k=4} 6
+""")
 
