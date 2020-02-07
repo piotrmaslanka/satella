1debadc0dfc8ff51c596fbe3eea45e258af39e52
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-07 10:27:09 +0100

    remove frozendict from requirements

--- tests/test_instrumentation/test_metrics/test_metric_data.py
+++ tests/test_instrumentation/test_metrics/test_metric_data.py
@@ -28,9 +28,9 @@ class TestMetricData(unittest.TestCase):
         self.assertTrue(MetricDataCollection(MetricData('root.metric', 10, {'service': 'my_service'}, 10)).strict_eq(a1))
 
     def test_update_4(self):
-        a1 = MetricDataCollection(MetricData('root.metric', 2, {'service': 'my_service'}))
-        a1 += MetricDataCollection(MetricData('root.metric', 10, {'service': 'my_service'}, 10))
-        self.assertTrue(MetricDataCollection(MetricData('root.metric', 10, {'service': 'my_service'}, 10)).strict_eq(a1))
+        a1 = MetricDataCollection(MetricData('root.metric', 2, {'service': 'my_service', 'slot': 1}))
+        a1 += MetricDataCollection(MetricData('root.metric', 10, {'service': 'my_service', 'slot': 1}, 10))
+        self.assertTrue(MetricDataCollection(MetricData('root.metric', 10, {'service': 'my_service', 'slot': 1}, 10)).strict_eq(a1))
 
     def test_update(self):
         a1 = MetricDataCollection(MetricData('root.metric', 25.0, {'period': 1}),
