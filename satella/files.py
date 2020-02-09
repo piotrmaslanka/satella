ac866bc4eb1cd534a6eadecd87c9e70b68410ad0
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-09 15:28:27 +0100

    a fix

--- satella/files.py
+++ satella/files.py
@@ -7,22 +7,22 @@ logger = logging.getLogger(__name__)
 __all__ = ['read_re_sub_and_write']
 
 
-def read_re_sub_and_write(path: str, pattern: tp.Union[re.Pattern, str],
+def read_re_sub_and_write(path: str, pattern: tp.Union[re.compile, str],
                           repl: tp.Union[tp.Callable[[re.Match], str]]) -> None:
     """
     Read a text file, treat with re.sub and write the contents
 
     :param path: path of file to treat
-    :param pattern: re.Pattern or a string, a pattern to match the file contents
+    :param pattern: regexp compiled pattern or a string, a pattern to match the file contents
     :param repl: string or a callable(str)->str to replace the contents
     """
     with open(path, 'r') as f_in:
         data = f_in.read()
 
-    if isinstance(pattern, re.Pattern):
-        data = pattern.sub(repl, data)
-    else:
+    if isinstance(pattern, str):
         data = re.sub(pattern, repl, data)
+    else:
+        data = pattern.sub(repl, data)
 
     with open(path, 'w') as f_out:
         f_out.write(data)
