cc5e2d277870bc68d960a10f9aee0eaf5ed000d1
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-10 14:14:29 +0100

    remove a useless piece of code

--- satella/posix/signals.py
+++ satella/posix/signals.py
@@ -28,24 +28,6 @@ def hang_until_sig(extra_signals: tp.Optional[tp.List[int]] = None,
     extra_signals = extra_signals or []
     global end
 
-    # Ascertain what Python are we working on. 2012 PyPy and earlier
-    # may be affected by https://bugs.pypy.org/issue1255
-
-    bugged_pypy = False
-    try:
-        import platform
-    except ImportError:
-        pass
-    else:
-        if platform.python_implementation() == 'PyPy':
-            try:
-                mon, day, year = platform.python_build()[1].split(' ')
-                year = int(year)
-            except (TypeError, ValueError):
-                pass
-            else:
-                bugged_pypy = year <= 2012
-
     signal.signal(signal.SIGTERM, __sighandler)
     signal.signal(signal.SIGINT, __sighandler)
     for s in extra_signals:
