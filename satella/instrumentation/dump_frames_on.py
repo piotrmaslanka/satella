60a4d8d04a58230b5717f05c287ac109e7115603
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-12 18:12:28 +0100

    small refactor in dump_frames_on

--- satella/instrumentation/dump_frames_on.py
+++ satella/instrumentation/dump_frames_on.py
@@ -1,23 +1,20 @@
-import logging
 import signal
 import sys
 import types
 import typing as tp
 
 try:
+    # noinspection PyProtectedMember
     SIG_TYPE = signal._SIG
 except AttributeError:
     SIG_TYPE = int  # we are running on Windows
 
-logger = logging.getLogger(__name__)
-
 
 # noinspection PyUnusedLocal
-# noinspection PyProtectedMember
 def dump_frames_on(sig_no: SIG_TYPE, stack_frame: types.FrameType, output: tp.TextIO):
     from satella.instrumentation import Traceback
 
-    output.write("Stack frame dump requested\n")
+    output.write("Stack frame dump requested in response to signal %s\n" % (sig_no, ))
     # noinspection PyProtectedMember
     for frame_no, frame in sys._current_frames().items():
         output.write("For stack frame %s" % (frame_no,))
@@ -26,7 +23,6 @@ def dump_frames_on(sig_no: SIG_TYPE, stack_frame: types.FrameType, output: tp.Te
     output.write("End of stack frame dump\n")
 
 
-# noinspection PyProtectedMember
 def install_dump_frames_on(signal_number: SIG_TYPE, output: tp.TextIO = sys.stderr):
     """
     Instruct Python to dump all frames onto output, along with their local variables
