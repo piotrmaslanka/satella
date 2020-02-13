ebcbc9b6d138288b86fe1b5e161aaaa65d1c97ea
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-13 03:42:37 +0100

    add test for precondition

--- tests/test_coding/test_debug.py
+++ tests/test_coding/test_debug.py
@@ -18,10 +18,10 @@ class TestTypecheck(unittest.TestCase):
         self.assertFalse(a(3))
 
     def test_precondition_none(self):
-        @precondition(a=None)
-        def do_any(a):
+        @precondition(a=None, b=lambda: True)
+        def do_any(a, b):
             return a
-        self.assertEqual(do_any(a=5), 5)
+        self.assertEqual(do_any(a=5, b=True), 5)
 
     def test_precondition_kwargs(self):
         @precondition(value='x == 2')
