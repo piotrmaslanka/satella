7244bf6ad89851b817412ba1068ef471b70a3ba5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-02-28 18:33:21 +0100

    this would've raised StopIteration earlier!

--- satella/coding/sequences/sequences.py
+++ satella/coding/sequences/sequences.py
@@ -40,10 +40,7 @@ def add_next(lst: tp.Iterable[T]) -> tp.Generator[tp.Tuple[T, tp.Optional[T]], N
     >>> list(add_next([1, 2, 3, 4, 5])) == [(1, 2), (2, 3), (3, 4), (4, 5), (5, None)]
     """
     iterator = iter(lst)
-    try:
-        prev_val = next(iterator)
-    except StopIteration:
-        return
+    prev_val = next(iterator)
     for val in iterator:
         yield prev_val, val
         prev_val = val
