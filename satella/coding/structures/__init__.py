73a95ce3c0b115542ae4fa2d66c42b40edf6ae0e
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-10 18:47:36 +0100

    fixed exports

--- satella/coding/structures/__init__.py
+++ satella/coding/structures/__init__.py
@@ -1,8 +1,8 @@
-from .dictionaries import *
+from .dictionaries import DictObject, apply_dict_object, DictionaryView
 from .immutable import Immutable, frozendict
 from .singleton import Singleton, SingletonWithRegardsTo
-from .structures import *
-from .typednamedtuple import *
+from .structures import SetHeap, OmniHashableMixin, TimeBasedHeap, Heap
+from .typednamedtuple import typednamedtuple
 
 __all__ = [
     'typednamedtuple',
