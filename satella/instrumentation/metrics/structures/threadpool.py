4dc5580e97d354d44c6f1d024dfcd50d0ead8798
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-04 03:27:47 +0200

    far more comprehensive testing

--- satella/instrumentation/metrics/structures/threadpool.py
+++ satella/instrumentation/metrics/structures/threadpool.py
@@ -1,4 +1,4 @@
-import atexit
+import threading
 import weakref
 from concurrent.futures import _base
 from concurrent.futures import thread
@@ -8,13 +8,10 @@ try:
 except ImportError:
     BrokenThreadPool = RuntimeError
 import typing as tp
-import threading
 
 from satella.time import measure
 
-from satella.instrumentation.metrics.metric_types import EmptyMetric, SummaryMetric, HistogramMetric, MetricLevel, \
-    CallableMetric
-from ..aggregate import AggregateMetric
+from satella.instrumentation.metrics.metric_types import EmptyMetric, MetricLevel, CallableMetric
 
 
 def _worker(executor_reference, work_queue, initializer, initargs):
@@ -125,3 +122,28 @@ class MetrifiedThreadPoolExecutor(ThreadPoolExecutor):
             self._work_queue.put(w)
             self._adjust_thread_count()
             return f
+
+
+    def _adjust_thread_count(self):
+        # if idle threads are available, don't spin new threads
+        if self._idle_semaphore.acquire(timeout=0):
+            return
+
+        # When the executor gets lost, the weakref callback will wake up
+        # the worker threads.
+        def weakref_cb(_, q=self._work_queue):
+            q.put(None)
+
+        num_threads = len(self._threads)
+        if num_threads < self._max_workers:
+            thread_name = '%s_%d' % (self._thread_name_prefix or self,
+                                     num_threads)
+            t = threading.Thread(name=thread_name, target=_worker,
+                                 args=(weakref.ref(self, weakref_cb),
+                                       self._work_queue,
+                                       self._initializer,
+                                       self._initargs))
+            t.daemon = True
+            t.start()
+            self._threads.add(t)
+            thread._threads_queues[t] = self._work_queue
