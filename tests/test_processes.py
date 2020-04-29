e266d28495c36a40afe2204d86794e690a982035
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-29 16:14:13 +0200

    fix test

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -6,8 +6,10 @@ from satella.processes import call_and_return_stdout
 class TestProcesses(unittest.TestCase):
     @unittest.skipIf('win' in sys.platform, 'Running on Windows')
     def test_return_stdout(self):
-        output = call_and_return_stdout('cat /proc/meminfo', shell=True, encoding='utf8')
-        self.assertIn('MemTotal', output)
-
         output = call_and_return_stdout('cat /proc/meminfo', shell=True)
         self.assertIn(b'MemTotal', output)
+
+    @unittest.skipIf('win' in sys.platform or sys.version_info.minor < 6, 'Running on Windows or Python 3.5')
+    def test_return_encoding(self):
+        output = call_and_return_stdout('cat /proc/meminfo', shell=True, encoding='utf8')
+        self.assertIn('MemTotal', output)
