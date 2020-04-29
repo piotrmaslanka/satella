9ec284cc9e5260f49f335fe5f07ae3848fcf1e80
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-29 16:25:45 +0200

    fix tests so that they won't hang on Travis

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -6,10 +6,10 @@ from satella.processes import call_and_return_stdout
 class TestProcesses(unittest.TestCase):
     @unittest.skipIf('win' in sys.platform, 'Running on Windows')
     def test_return_stdout(self):
-        output = call_and_return_stdout('cat /proc/meminfo', shell=True, timeout=3)
-        self.assertIn(b'MemTotal', output)
+        output = call_and_return_stdout('ls /', shell=True, timeout=3)
+        self.assertIn(b'usr', output)
 
     @unittest.skipIf('win' in sys.platform or sys.version_info.minor < 6, 'Running on Windows or Python 3.5')
     def test_return_encoding(self):
-        output = call_and_return_stdout('cat /proc/meminfo', shell=True, encoding='utf8', timeout=3)
-        self.assertIn('MemTotal', output)
+        output = call_and_return_stdout('ls /', shell=True, encoding='utf8', timeout=3)
+        self.assertIn('usr', output)
