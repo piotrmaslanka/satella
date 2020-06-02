ad77e1403f6250d4b7aad52ff8b613829c37f5f0
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-06-02 16:30:32 +0200

    mild refactor in log_exceptions

--- satella/coding/recast_exceptions.py
+++ satella/coding/recast_exceptions.py
@@ -48,7 +48,9 @@ class log_exceptions:
     """
     __slots__ = ('logger', 'severity', 'format_string', 'locals', 'exc_types')
 
-    def __init__(self, logger, severity=logging.ERROR, format_string='{e}',
+    def __init__(self, logger: logging.Logger,
+                 severity: int = logging.ERROR,
+                 format_string: str = '{e}',
                  locals_: tp.Optional[tp.Dict] = None,
                  exc_types: tp.Optional[tp.Union[ExcType, tp.Sequence[ExcType]]] = None):
         self.logger = logger
