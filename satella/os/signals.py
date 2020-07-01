9188cd56abcec3e3fcb5e0153da08904875f78bd
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-07-01 17:35:47 +0200

    restore previous handlers

--- satella/os/signals.py
+++ satella/os/signals.py
@@ -21,19 +21,24 @@ def hang_until_sig(extra_signals: tp.Optional[tp.Sequence[int]] = None) -> None:
     global end
     extra_signals = extra_signals or ()
 
+    old_term = signal.getsignal(signal.SIGTERM)
+    old_int = signal.getsignal(signal.SIGINT)
+    olds = []
+
     # Set the signal handler
     signal.signal(signal.SIGTERM, __sighandler)
     signal.signal(signal.SIGINT, __sighandler)
     for s in extra_signals:
+        olds.append(signal.getsignal(s))
         signal.signal(s, __sighandler)
 
     while not end:
         sleep(0.5, True)
 
     # Unset the signal handler
-    signal.signal(signal.SIGTERM, signal.SIG_DFL)
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    for s in extra_signals:
-        signal.signal(s, signal.SIG_DFL)
+    signal.signal(signal.SIGTERM, old_term)
+    signal.signal(signal.SIGINT, old_int)
+    for s, old_handler in zip(extra_signals, olds):
+        signal.signal(s, old_handler)
 
     end = False  # reset for next use
