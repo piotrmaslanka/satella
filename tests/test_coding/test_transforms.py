9889238e9874fbc4d125ead27b1082b2e78438b4
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-08-13 23:18:52 +0200

    extra unit tests for merge_series

--- tests/test_coding/test_transforms.py
+++ tests/test_coding/test_transforms.py
@@ -4,7 +4,56 @@ from satella.coding.transforms import stringify, split_shuffle_and_join, one_tup
     merge_series
 
 
-class MyTestCase(unittest.TestCase):
+class TestTransforms(unittest.TestCase):
+    def test_series_merger(self):
+        s1 = [(100, 1), (200, 2), (300, 3)]
+        s2 = ((100, 2), (200, 3), (300, 4))
+
+        sm = merge_series(s1, s2)
+        self.assertEqual(next(sm), (100, 1, 2))
+        self.assertEqual(next(sm), (200, 2, 3))
+        self.assertEqual(next(sm), (300, 3, 4))
+        self.assertRaises(StopIteration, lambda: next(sm))
+
+    def test_series_merger2(self):
+        s1 = [(100, 1), (200, 2), (300, 3)]
+        s2 = ((101, 2), (201, 3), (301, 4))
+
+        sm = merge_series(s1, s2)
+        self.assertEqual(next(sm), (101, 1, 2))
+        self.assertEqual(next(sm), (200, 2, 2))
+        self.assertEqual(next(sm), (201, 2, 3))
+        self.assertEqual(next(sm), (300, 3, 3))
+        self.assertEqual(next(sm), (301, 3, 4))
+        self.assertRaises(StopIteration, lambda: next(sm))
+
+    def test_series_merger_3(self):
+        s1 = [(100, 1), (200, 2)]
+        s2 = []
+
+        sm = merge_series(s1, s2)
+
+        self.assertRaises(StopIteration, lambda: next(sm))
+
+    def test_series_merger_4(self):
+        s1 = [(100, 1), (200, 2), (300, 3)]
+        s2 = [(100, 1)]
+
+        sm = merge_series(s1, s2)
+        self.assertEqual(next(sm), (100, 1, 1))
+        self.assertEqual(next(sm), (200, 2, 1))
+        self.assertEqual(next(sm), (300, 3, 1))
+
+        self.assertRaises(StopIteration, lambda: next(sm))
+
+    def test_series_merger_5(self):
+        s1 = [(100, 1), (200, 2), (300, 3)]
+        sm = merge_series(s1)
+
+        self.assertEqual(next(sm), (100, 1))
+        self.assertEqual(next(sm), (200, 2))
+        self.assertEqual(next(sm), (300, 3))
+        self.assertRaises(StopIteration, lambda: next(sm))
 
     def test_merge_series(self):
         s1 = [(10, 'A'), (20, 'B')]
