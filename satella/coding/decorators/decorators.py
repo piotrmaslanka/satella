451f4e256abd2354840ae528e330cfc8a3c8b43f
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-09-21 22:04:07 +0200

    remove an assignment to __wrapped__

--- satella/coding/decorators/decorators.py
+++ satella/coding/decorators/decorators.py
@@ -91,7 +91,6 @@ def wraps(cls_to_wrap: tp.Type) -> tp.Callable[[tp.Type], tp.Type]:
             cls.__module__ = cls_to_wrap.__module__
         if hasattr(cls_to_wrap, '__annotations__'):
             cls.__annotations__ = cls_to_wrap.__annotations__
-        cls.__wrapped__ = cls
         return cls
 
     return outer
