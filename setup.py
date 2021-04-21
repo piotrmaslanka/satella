117b4f30aea7789f998d2bf227cec3f16ea1b4ec
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-04-21 17:06:22 +0200

    moved some stuff from setup.py into setup.cfg

--- setup.py
+++ setup.py
@@ -2,14 +2,9 @@ from setuptools import setup, find_packages
 from satella import __version__
 
 
-setup(keywords=['ha', 'high availability', 'scalable', 'scalability', 'server', 'metrics'],
-      packages=find_packages(include=['satella', 'satella.*']),
+setup(packages=find_packages(include=['satella', 'satella.*']),
       version=__version__,
-      install_requires=[
-            'psutil'
-      ],
       test_suite='nose2.collector.collector',
-      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       extras_require={
             'HTTPJSONSource': ['requests'],
             'YAMLSource': ['pyyaml'],
