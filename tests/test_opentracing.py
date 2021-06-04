a89f597b842aaf2fa14c1a09e30b99767e24f1f7
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 18:53:48 +0200

    more coverage

--- tests/test_opentracing.py
+++ tests/test_opentracing.py
@@ -6,3 +6,5 @@ from satella.opentracing import trace_exception
 class TestOpentracing(unittest.TestCase):
     def test_trace_exception_none(self):
         trace_exception(None)
+
+        trace_exception(5)
