bb6571a8dca81df15e33c7aa75dc87f3d076ae25
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 17:51:15 +0200

    move package list to setup.cfg

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from satella import __version__
 
 
-setup(packages=find_packages(include=['satella', 'satella.*']),
-      version=__version__,
-      test_suite='nose2.collector.collector'
-      )
+setup(version=__version__)
