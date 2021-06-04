086d1a66ceed4d30e52a0bdd26b045262f619192
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 18:56:41 +0200

    more coverage

--- tests/test_opentracing.py
+++ tests/test_opentracing.py
@@ -1,10 +1,18 @@
 import unittest
 
 from satella.opentracing import trace_exception
+from unittest import mock
 
 
 class TestOpentracing(unittest.TestCase):
     def test_trace_exception_none(self):
         trace_exception(None)
 
-        trace_exception(5)
+        span = mock.Mock()
+        try:
+            raise ValueError()
+        except ValueError:
+            trace_exception(span)
+
+        span.set_tag.assert_called_with('error', True)
+        span.log_kv.assert_called()
