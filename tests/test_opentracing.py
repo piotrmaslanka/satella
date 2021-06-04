d603be6163ece854147c3caa73b8f23718bd98c5
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 19:13:33 +0200

    more coverage

--- tests/test_opentracing.py
+++ tests/test_opentracing.py
@@ -1,12 +1,36 @@
 import unittest
 
 from satella.coding.concurrent import call_in_separate_thread
-from satella.opentracing import trace_exception, trace_future
+from satella.opentracing import trace_exception, trace_future, trace_function
 from unittest import mock
 
 
 class TestOpentracing(unittest.TestCase):
-    def test_trace_future(self):
+    def test_trace_function(self):
+        class MockTracer:
+            def __init__(self):
+                self.start_active_span_called = True
+
+            def start_active_span(self, *args, **kwargs):
+                class MockScope(mock.Mock):
+                    def __enter__(self):
+                        return self
+
+                    def __exit__(self, exc_type, exc_val, exc_tb):
+                        return False
+                self.start_active_span_called = True
+                return MockScope()
+
+        tracer = MockTracer()
+
+        @trace_function(tracer, 'trace_me')
+        def trace_me():
+            pass
+
+        trace_me()
+        self.assertTrue(tracer.start_active_span_called)
+
+    def test_trace_future_success(self):
         @call_in_separate_thread()
         def test_me():
             pass
@@ -15,6 +39,18 @@ class TestOpentracing(unittest.TestCase):
         span = mock.Mock()
         trace_future(fut, span)
         fut.result()
+        fut.thread.join()
+        span.finish.assert_called()
+
+    def test_trace_future_exception(self):
+        @call_in_separate_thread()
+        def fail_me():
+            raise ValueError()
+
+        fut = fail_me()
+        span = mock.Mock()
+        trace_future(fut, span)
+        self.assertRaises(ValueError, fut.result)
         fut.thread.join()
         span.finish.assert_called()
 
