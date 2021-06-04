4e40979c23772d3a55bd97cf0206c675f24eb974
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-04 17:55:02 +0200

    revert setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from satella import __version__
 
 
-setup(version=__version__)
+setup(packages=find_packages(include=['satella', 'satella.*']),
+      version=__version__)
