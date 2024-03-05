62a831f20af4839300ea9a5ccfcfc1fae4e9d249
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2024-03-05 17:11:30 +0100

    fix unit test

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -12,9 +12,9 @@ class TestProcesses(unittest.TestCase):
     @unittest.skipIf('win' in sys.platform, 'Running on Windows')
     def test_return_stdout(self):
         output = call_and_return_stdout('ls .', shell=True, timeout=3, expected_return_code=0)
-        self.assertIn(b'requirements.txt', output)
+        self.assertIn(b'pyproject.toml', output)
 
     @unittest.skipIf('win' in sys.platform or sys.version_info.minor < 6, 'Running on Windows or Python 3.5')
     def test_return_encoding(self):
         output = call_and_return_stdout('ls .', shell=True, encoding='utf8', timeout=3)
-        self.assertIn('requirements.txt', output)
+        self.assertIn('pyproject.toml', output)
